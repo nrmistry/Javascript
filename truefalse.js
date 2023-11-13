01196ba678dbbd7a114012db1c6c394068a77498
nrmistry <nilum@outlook.com>
2023-11-13 03:34:16 +0000

    true/false function

---
+++ truefalse.js
@@ -0,0 +1,7 @@
+function isPositive(number){
+    return number >0;
+}
+
+//Example of use, input number to check whether it's positive or negative
+console.log(isPositive(5));
+console.log(isPositive(-3));
